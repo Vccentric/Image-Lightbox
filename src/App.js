55e973f404464f1260e81ac76a0cef0ed7462519
Vccentric <35283237+Vccentric@users.noreply.github.com>
2018-10-14 18:15:38 -0700

    1. Add css file to project. 2. Made changes to 'app' id.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
 import ImageGrid from './ImageGrid';
+import './image-lightbox.css';
 
 function App(props) {
     return (
-        <div className="App">
+        <div id="app">
             <h1>Image Lightbox!!!</h1>
             <ImageGrid />
         </div>
