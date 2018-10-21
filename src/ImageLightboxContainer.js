c3dc2f7e01dfe8ce4aec4bdaf2882eed93c236e5
Vccentric <35283237+Vccentric@users.noreply.github.com>
2018-10-20 17:01:39 -0700

    1. Use a public Flickr API to get sample images to showcase this plugin. 2. Add support for 'loading' spinner icon.

--- src/ImageLightboxContainer.js
+++ src/ImageLightboxContainer.js
@@ -17,11 +17,41 @@ class ImageLightboxContainer extends React.Component {
         this.clickThumbnail = this.clickThumbnail.bind(this);
         this.closeLightbox = this.closeLightbox.bind(this);
         this.changeSelectedImage = this.changeSelectedImage.bind(this);
+        this.getData = this.getData.bind(this);
         this.state = {
             lightboxOpen: false,
-            images: data,
+            images: [],
             selectedImage: null,
+            title: 'Image Lightbox',
+            firstLoad: true,
         };
+    }
+
+    componentDidMount() {
+        this.getData(); // get Flickr data
+    }
+
+    // function to make ajax call to get data from url (Flickr feed)
+    getData() {
+        // set url to get public Flickr feed
+        const url = 'https://cors-anywhere.herokuapp.com/https://api.flickr.com/services/feeds/photos_public.gne?tags=puppies&format=json&nojsoncallback=true';
+        const images = [];
+        fetch(url)
+            .then(response => response.json()) // convert response into json
+            .then((jsonData) => {
+                const items = (jsonData && jsonData.items) ? jsonData.items : [];
+                const title = (jsonData && jsonData.title) ? `Flickr Public Feed: ${jsonData.title}` : 'Flickr Public Feed: ';
+                for (let i = 0; i < items.length; i++) { // convert json into an appropriate 'key/value' object array
+                    const src = items[i].media.m;
+                    const alt = items[i].title;
+                    images.push({ src, alt });
+                }
+                this.setState({ images, title, firstLoad: false });
+            })
+            .catch((error) => {
+                console.log('Error has occurred:');
+                console.log(error);
+            });
     }
 
     // function to handle clicking on thumbnail
@@ -41,10 +71,13 @@ class ImageLightboxContainer extends React.Component {
     }
 
     render() {
-        const { lightboxOpen, selectedImage, images } = this.state;
+        const {
+            lightboxOpen, selectedImage, images, title, firstLoad,
+        } = this.state;
         return (
             <div id="image-lightbox-container">
-                <h1>Image Lightbox!!!</h1>
+                <h1 id="title">{title}</h1>
+                {firstLoad && <div id="loadingSpinner">Loading...</div>}
                 <ImageGrid
                     images={images}
                     handleClickThumbnail={this.clickThumbnail}
