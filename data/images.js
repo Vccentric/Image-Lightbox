19621b5e1e4ce1752dc4ef63a5ad1beb96dda661
Vccentric <35283237+Vccentric@users.noreply.github.com>
2022-11-21 18:56:37 -0800

    Move images data into its own separate file

---
+++ data/images.js
@@ -0,0 +1,11 @@
+import img1 from "../public/images/img_mountains_wide.jpg";
+import img2 from "../public/images/img_nature_wide.jpg";
+import img3 from "../public/images/img_snow_wide.jpg";
+
+export const images = [
+  { src: img1, alt: "Spring Mountains" },
+  { src: img2, alt: "Fall Mountains" },
+  { src: img3, alt: "Winter Mountains" },
+];
+
+export default images;
--- src/index.js
+++ src/index.js
@@ -3,16 +3,9 @@ import { createRoot } from "react-dom/client";
 import ImageLightbox from "./containers/ImageLightbox";
 import "normalize.css";
 import "./styles/image-lightbox.scss";
-import img1 from "../public/images/img_mountains_wide.jpg";
-import img2 from "../public/images/img_nature_wide.jpg";
-import img3 from "../public/images/img_snow_wide.jpg";
+import images from "../data/images";
 
-// local sample images
-const images = [
-  { src: img1, alt: "Spring Mountains" },
-  { src: img2, alt: "Fall Mountains" },
-  { src: img3, alt: "Winter Mountains" },
-];
+// setup sample images
 const data = { images };
 
 const container = document.getElementById("root");
